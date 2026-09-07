a16850bbfd7c9591cda0662c66ae7b53716b5b4b
Qoder-Undefined <kamalaliyu503@gmail.com>
2026-09-07 16:09:22 +0100

    update

--- bakery/bakery/middleware/upload.js
+++ bakery/bakery/middleware/upload.js
@@ -34,7 +34,7 @@ const fileFilter = (req, file, cb) => {
 // Multer upload instance
 const upload = multer({
   storage,
-  limits: { fileSize: 2 * 1024 * 1024 }, // 2MB limit
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
   fileFilter
 });
 
